34d973ab9de06235f997b5e8088b0853ffcd6303
Abhishaike <abhishaike@gmail.com>
2016-12-26 00:49:08 -0600

    Update VoiceScrapper(GMU).py

--- VoiceScrapper(GMU).py
+++ VoiceScrapper(GMU).py
@@ -20,63 +20,8 @@ import urllib2
 import requests 
 
 
-INDIAN = ["http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=207",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=587",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=593",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=910",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=1017",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=1437",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=1697",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=1966",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=2206",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=2209",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=2236",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=2242",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=2303",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=2353",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=2354"]
-
-
-
-AMERICAN = ["http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=61",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=110",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=132",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=73",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=74",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=76",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=81",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=83",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=84",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=92",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=95",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=105",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=106",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=109",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=112"]
-
-'''
-TEST DATA:
-
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=114",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=120",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=122",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=124",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=125",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=127",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=128",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=129",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=130",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=133",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=134",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=137",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=138",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=142",
-"http://accent.gmu.edu/searchsaa.php?function=detail&speakerid=145"]
-
-'''
-
 IndianCombined = AudioSegment.empty()
-for x in INDIAN:
+for x in FILE:
 	page = urllib.urlopen(x, 'lxml')
 	html = page.read()
 	soup = BeautifulSoup(html)
