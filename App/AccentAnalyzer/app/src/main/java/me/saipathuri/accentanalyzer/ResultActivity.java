8939cbf4e46f76de9028ab3f26dd0d0b9ac7bc13
Sai Pathuri <saipathuri@gmail.com>
2017-09-18 21:00:52 -0500

    remove selected language, add button to start again

--- App/AccentAnalyzer/app/src/main/java/me/saipathuri/accentanalyzer/ResultActivity.java
+++ App/AccentAnalyzer/app/src/main/java/me/saipathuri/accentanalyzer/ResultActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -14,19 +15,28 @@ public class ResultActivity extends AppCompatActivity {
 
     private TextView mResultTextView;
     private TextView mErrorTextView;
+    private Button mStartOverButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        Intent intentThatStartedThisActivty = getIntent();
+        final Intent intentThatStartedThisActivty = getIntent();
 
         mResult = intentThatStartedThisActivty.getStringExtra(Constants.RESULTS_KEY);
         mError = intentThatStartedThisActivty.getStringExtra(Constants.ERROR_KEY);
 
         mResultTextView = (TextView) findViewById(R.id.tv_result_result);
         mErrorTextView = (TextView) findViewById(R.id.tv_result_error);
+        mStartOverButton = (Button) findViewById(R.id.btn_start_over);
+        mStartOverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentToStartMainActivity = new Intent(ResultActivity.this, MainActivity.class);
+                startActivity(intentThatStartedThisActivty);
+            }
+        });
 
         mResultTextView.setText(mResult);
         mErrorTextView.setText(mError);
